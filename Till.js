40e8e6e50b82ea45d85420da893f1eb673e2b33d
jbulger93 <j-bulger@hotmail.co.uk>
2017-06-28 20:55:33 +0100

    five products added

---
+++ Till.js
@@ -0,0 +1,23 @@
+var apples = {
+name: 'Apples'
+price: 0.99
+}
+var pears = {
+name: 'Pears'
+price: 0.20
+}
+var bannanas = {
+name: 'Bannanas'
+price: 0.75
+}
+var oranges = {
+name: 'Oranges'
+price: 0.50
+}
+var melons = {
+name: 'Melons'
+price: 1.99
+}
+
+
+var products = [apples] [pears] [bannanas] [oranges] [melons]
